9714bd71f8d98a858daa7569799373e599531a54
PatrickO10 <patrickorth10@gmail.com>
2014-11-07 20:53:45 -0600

    interactive resume

---
+++ js/helper.js
@@ -0,0 +1,328 @@
+/*
+
+This file contains all of the code running in the background that makes resumeBuilder.js possible.
+ We call these helper functions 
+because they support your code in this course.
+
+Don't worry, you'll learn what's going on in this file throughout the course.
+ You won't need to make any changes to it until you 
+start experimenting with inserting a Google Map in Problem Set 3.
+
+Cameron Pittman
+*/
+
+
+/*
+These are HTML strings. As part of the course, you'll be using JavaScript functions
+replace the %data% placeholder text you see in them.
+*/
+var HTMLheaderName = "<h1 id='name'>%data%</h1>";
+var HTMLheaderRole = "<span class='white-text'>%data%</span><hr/>";
+
+var HTMLcontactGeneric = "<li class='flex-item'><span class='white-text'>%contact%</span><span class='light-teal-text'>%data%</span></li>";
+var HTMLmobile = "<li class='flex-item'><span class='white-text'><i class='ion-android-call'></i> mobile</span><span class='light-teal-text'>%data%</span></li>";
+var HTMLemail = "<li class='flex-item'><span class='white-text'><i class='ion-email'></i> email</span><span class='light-teal-text'>%data%</span></li>";
+var HTMLtwitter = "<li class='flex-item'><span class='white-text'><i class='ion-social-twitter'></i> twitter</span><a href='https://twitter.com/patrick_orth' class='contacts-a'><span class='light-teal-text'>%data%</span></a></li>";
+var HTMLgithub = "<li class='flex-item'><span class='white-text'><i class='ion-social-github'></i> github</span><a href='https://github.com/PatrickO10' class='contacts-a'><span class='light-teal-text'>%data%</span></a></li>";
+var HTMLblog = "<li class='flex-item'><span class='white-text'><i class='ion-android-book'></i> blog</span><a href='http://findingthingsout.blogspot.com/' class='contacts-a'><span class='light-teal-text'>%data%</span></a></li>";
+var HTMLlocation = "<li class='flex-item'><span class='white-text'><i class='ion-map'></i> location</span><span class='light-teal-text'>%data%</span></li>";
+var HTMLlinkedin = "<li class='flex-item'><span class='white-text'><i class='ion-social-linkedin'></i> linkedIn</span><a href='https://www.linkedin.com/pub/patrick-orth/97/91a/221' class='contacts-a'><span class='light-teal-text'>%data%</span></a></li>";
+
+
+var HTMLbioPic = "<img src='%data%' class='biopic'>";
+var HTMLWelcomeMsg = "<span class='welcome-message'>%data%</span>";
+
+var HTMLskillsStart = "<h3 id='skillsH3'>Skills at a Glance:</h3><ul id='skills' class='flex-box'></ul>";
+var HTMLskills = "<li class='flex-item'><span class='light-teal-text'>%data%</span></li>";
+
+var HTMLworkStart = "<div class='work-entry'></div>";
+var HTMLworkEmployer = "<a href='%url%'>%data%";
+var HTMLworkTitle = " - %data%</a>";
+var HTMLworkDates = "<div class='date-text'>%data%</div>";
+var HTMLworkLocation = "<div class='location-text'>%data%</div>";
+var HTMLworkDescription = "<p><br>%data%</p>";
+
+var HTMLprojectStart = "<div class='project-entry'></div>";
+var HTMLprojectTitle = "<a href='%url%'>%data%</a>";
+var HTMLprojectDates = "<div class='date-text'>%data%</div>";
+var HTMLprojectDescription = "<p><br>%data%</p>";
+var HTMLprojectImage = "<img src='%data%' class='img-content'>";
+
+var HTMLschoolStart = "<div class='education-entry'></div>";
+var HTMLschoolName = "<a href='%url%'>%data%";
+var HTMLschoolDegree = " -- %data%</a>";
+var HTMLschoolDates = "<div class='date-text'>%data%</div>";
+var HTMLschoolLocation = "<div class='location-text'>%data%</div>";
+var HTMLschoolMajor = "<em><br>Major: %data%</em>";
+var HTMLschoolMinor = "<em><br>Minor: %data%</em>";
+
+
+var HTMLonlineClasses = "<h3>Online Classes</h3>";
+var HTMLonlineTitle = "<a href='%url%'>%data%";
+var HTMLonlineSchool = " - %data%</a>";
+var HTMLonlineDates = "<div class='date-text'>%data%</div>";
+var HTMLonlineURL = "<br><a href='%url%'>%data%</a>";
+var HTMLonlineDescription = "<p><br>%data%</p>"
+
+var internationalizeButton = "<button>Internationalize</button>";
+var googleMap = "<div id='map'></div>";
+var HTMLmapbgImg = "<img class='mapbgimg' src='%data%'>"; // var that will add background image to mapDiv
+
+
+/*
+The International Name challenge in Lesson 2 where you'll create a function that will need this 
+helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
+*/
+$(document).ready(function() {
+  $('button').click(function() {
+    var iName = inName('Patrick Orth') || function(){};
+    $('#name').html(iName);  
+  });
+})
+
+
+/*
+The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
+*/
+clickLocations = [];
+
+function logClicks(x,y) {
+  clickLocations.push(
+    {
+      "x": x,
+      "y": y
+    }
+  );
+  console.log("x location: " + x + "; y location: " + y);
+}
+
+$(document).click(function(loc) {
+  // your code goes here!
+  var x = loc.pageX;
+  var y = loc.pageY;
+
+  logClicks(x,y);
+});
+
+
+var map; // declares a global map variable
+// var panorama; // declares a global panorama variable
+// var sv = new google.maps.StreetViewService();
+
+
+var panorama;
+
+/*
+Start here! initializeMap() is called when page is loaded.
+*/
+
+function initializeMap() {
+  var sv = new google.maps.StreetViewService();
+  var locations;
+
+
+  var mapOptions = {
+    scrollwheel: false,
+    disableDefaultUI: true
+  };
+
+
+  // This next line makes `map` a new Google Map JavaScript Object and attaches it to
+  // <div id="map">, which is appended as part of an exercise late in the course.
+  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+
+
+
+  /*
+  locationFinder() returns an array of every location string from the JSONs
+  written for bio, education, and work.
+  */
+  function locationFinder() {
+
+    // initializes an empty array
+    var locations = [];
+
+    // adds the single location property from bio to the locations array
+    locations.push(bio.contacts.location);
+
+    // iterates through school locations and appends each location to
+    // the locations array
+    for (var school in education.schools) {
+      locations.push(education.schools[school].location);
+    }
+
+    // iterates through work locations and appends each location to
+    // the locations array
+    for (var job in work.jobs) {
+      locations.push(work.jobs[job].location);
+    }
+
+    return locations;
+  }
+
+  /*
+  createMapMarker(placeData) reads Google Places search results to create map pins.
+  placeData is the object returned from search results containing information
+  about a single location.
+
+  I added a background image and a streetview panorama map when you click on a pin.
+  */
+  function createMapMarker(placeData) {
+
+    // The next lines save location data from the search result object to local variables
+    var lat = placeData.geometry.location.k; // latitude from the place service
+    var lon = placeData.geometry.location.B; // longitude from the place service
+    var name = placeData.formatted_address; // name of the place from the place service
+    var bounds = window.mapBounds; // current boundaries of the map window
+
+    // added variables to implement my background streetview image
+    var address = lat + ',' + lon; // lat/lng for google maps background image
+    var panoramaLoc = new google.maps.LatLng(lat, lon); // location of the panorama
+
+    // panoramaOptions is an object with the specific streetview and point-of-view of the panorama
+    var panoramaOptions = {
+      position: panoramaLoc,
+      pov: {
+          heading: 34,
+          pitch: 10
+          }
+      };
+      var pinImg = 'images/Map-Marker-Ball-Azure-icon.png';
+
+    // marker is an object with additional data about the pin for a single location
+    var marker = new google.maps.Marker({
+      map: map,
+      position: placeData.geometry.location,
+      icon: pinImg,
+      title: name
+    });
+
+    // infoWindows are the little helper windows that open when you click
+    // or hover over a pin on a map. They usually contain more information
+    // about a location.
+    var infoWindow = new google.maps.InfoWindow({
+      content: name
+    });
+
+    
+    /*
+    function initializePano() {
+    // var fenway = new google.maps.LatLng(42.345573, -71.098326);
+    
+
+    var panoramaOptions = {
+      position: panoramaLoc,
+      pov: {
+        heading: 34,
+        pitch: 10
+      }
+    };
+    var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
+    map.setStreetView(panorama);
+    }
+*/  
+    // hmmmm, I wonder what this is about...
+    google.maps.event.addListener(marker, 'click', function(event) {
+      infoWindow.open(map, marker, svBackground(address));
+      sv.getPanoramaByLocation(event.latLng, 50, processSVData);
+      
+      //google.maps.event.addDomListener(window, 'load', initializePano);
+      // creates a new streetviewPanorama for that pin when you click on it
+      //panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
+/*
+      // streetviewUrl gets the url where the streetview is located
+      var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=640x400&location=' + address + '&pitch=-0.76' + '';
+      console.log(streetviewUrl);
+      var formattedImg = HTMLmapbgImg.replace('%data%', streetviewUrl); // replaces data
+      $('body').append(formattedImg); // appends it to body
+      //sv.getPanoramaByLocation(event.latLng, 50, processSVData);*/
+    });
+
+
+    function processSVData(data, status) {
+      if (status == google.maps.StreetViewStatus.OK) {
+        var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'));
+        panorama.setPano(data.location.pano);
+        panorama.setPov({
+          heading: 25,
+          pitch: -0.76
+          });
+        panorama.setVisible(true);
+    } else {
+      alert('Street View data not found for this location.');
+      }
+    }
+
+
+    // this is where the pin actually gets added to the map.
+    // bounds.extend() takes in a map location object
+    bounds.extend(new google.maps.LatLng(lat, lon));
+    // fit the map to the new marker
+    map.fitBounds(bounds);
+    // center the map
+    map.setCenter(bounds.getCenter());
+  }
+
+  /*
+  callback(results, status) makes sure the search returned results for a location.
+  If so, it creates a new map marker for that location.
+  */
+  function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      createMapMarker(results[0])
+    }
+  }
+
+
+  /*
+  pinPoster(locations) takes in the array of locations created by locationFinder()
+  and fires off Google place searches for each location
+  */
+
+  function pinPoster(locations) {
+
+    // creates a Google place search service object. PlacesService does the work of
+    // actually searching for location data.
+    var service = new google.maps.places.PlacesService(map);
+
+    // Iterates through the array of locations, creates a search object for each location
+    for (place in locations) {
+
+      // the search request object
+      var request = {
+        query: locations[place]
+      }
+
+      // Actually searches the Google Maps API for location data and runs the callback 
+      // function with the search results after each search.
+      service.textSearch(request, callback);
+    }
+  }
+
+  // Sets the boundaries of the map based on pin locations
+  window.mapBounds = new google.maps.LatLngBounds();
+
+  // locations is an array of location strings returned from locationFinder()
+  locations = locationFinder();
+
+  // pinPoster(locations) creates pins on the map for each location in
+  // the locations array
+  pinPoster(locations);
+
+};
+
+    //google.maps.event.addDomListener(window, 'load', initializePano); //test
+/*
+Uncomment all the code below when you're ready to implement a Google Map!
+*/
+
+// Calls the initializeMap() function when the page loads
+window.addEventListener('load', initializeMap);
+
+// Vanilla JS way to listen for resizing of the window 
+// and adjust map bounds
+window.addEventListener('resize', function(e) {
+  // Make sure the map bounds get updated on page resize
+  map.fitBounds(mapBounds);
+});
+
---
+++ js/resumeBuilder.js
@@ -0,0 +1,339 @@
+// For each section I put JSON, display function, and calling the function
+// because I want the code to be next to each other when working on them.
+
+var work = {
+    "jobs": [
+        {
+            "employer": "USBANK",
+            "title": "Teller",
+            "location": "9633 Lyndale Ave S, Bloomington MN",
+            "dates": "December 2012 to December 2013",
+            "description": "Sales, customer service, control of branch money"
+        },
+        {
+            "employer": "MARATHON PETROLEUM COMPANY (Now Northern Tier Energy)",
+            "title": "Summer Hire",
+            "location": "44.867259,-93.000497",
+            "city": "St. Paul Park",
+            "dates": ["May-September 2009", "May-September 2010"],
+            "description": "general maintenance, lawn care, painting, fueling equipment/trucks"
+        },
+    ]
+};
+
+work.display = function() {
+  for (var job in work.jobs) {
+    $("#workExperience").append(HTMLworkStart);
+    var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+    var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
+    var formattedEmployerTitle = formattedEmployer + formattedTitle;
+    $(".work-entry:last").append(formattedEmployerTitle);
+
+    var formattedDates = HTMLworkDates.replace("%data%",work.jobs[job].dates);
+    $(".work-entry:last").append(formattedDates);
+
+    var formattedDescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
+    $(".work-entry:last").append(formattedDescription);
+  }
+};
+
+work.display();
+
+var bio = {
+	"name": "Patrick Orth",
+	"role": "Web Developer",
+	"contacts": {
+    "mobile": "612-203-1916",
+		"email": "patrickorth10@gmail.com",
+		"github": "PatrickO10",
+		"twitter": "@patrick_orth",
+    "blog": "my blog",
+    "linkedin": "my linkedin",
+		"location": "Edina, MN"
+	},
+	"welcomeMessage" : " Welcome to my resume",
+	"skills" : ["Javascript", "HTML", "CSS", "Python"],
+	"bioPic" : "images/profilepic.jpg"
+};
+
+bio.display = function() {
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    $("#header").prepend(formattedName + formattedRole);
+
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
+    var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+    var formattedBlog = HTMLblog.replace("%data%", bio.contacts.blog);
+    var formattedLinkedin = HTMLlinkedin.replace("%data%", bio.contacts.linkedin);
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+    $("#topContacts").append(formattedMobile + formattedEmail + formattedGitHub + formattedTwitter + formattedLinkedin + formattedBlog + formattedLocation);
+    $("#footerContacts").append(formattedMobile + formattedEmail + formattedGitHub + formattedLinkedin + formattedTwitter + formattedLocation);
+    var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+    $("#header").append(formattedBioPic);
+    var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+    $("#header").append(formattedWelcomeMsg);
+    $("#header").append(HTMLskillsStart);
+    if (bio.skills.length > 0) {
+      for (var skill in bio.skills) {
+        var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+        $("#skills").append(formattedSkill);
+    }
+  }
+};
+
+bio.display();
+
+// Projects 
+var projects = {
+    "projects" : [
+    {
+    "title": "Mock Portfolio",
+    "dates": "2014",
+    "description": "Learned HTML and CSS by creating a responsive website.",
+    "images": ["images/mockimg (2).png", "images/mockimg.png", "images/mockcodeimg.png"]
+    },
+    {
+      "title": "Interactive Resume",
+      "dates": "2014",
+      "description": "Learned Javascript and how to use it with HTML and CSS",
+      "images": ["images/197x148.gif", "images/197x148.gif", "images/197x148.gif"]
+    },
+    {
+      "title": "CS101 project",
+      "dates": "2014",
+      "description": "Learned python while building a network around strings and manipulating said network",
+      "images": ["images/197x148.gif", "images/197x148.gif", "images/197x148.gif"]
+    }
+  ]
+};
+
+projects.display = function() {
+    for (var project in projects.projects) {
+     $("#projects").append(HTMLprojectStart);
+     var formattedProjectTitle = HTMLprojectTitle.replace("%data%",projects.projects[project].title);
+     $(".project-entry:last").append(formattedProjectTitle);
+     var formattedProjectDates = HTMLprojectDates.replace("%data%",projects.projects[project].dates);
+     $(".project-entry:last").append(formattedProjectDates);
+     var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+     $(".project-entry:last").append(formattedProjectDescription);
+
+     if (projects.projects[project].images.length > 0) {
+      for (var image in projects.projects[project].images) {
+       var formattedImage = HTMLprojectImage.replace("%data%",projects.projects[project].images[image]); 
+       $(".project-entry:last").append(formattedImage);
+    }
+
+        }
+ 
+    }
+};
+
+projects.display();
+
+
+var education = {
+  "schools": [
+  {
+    "name": "Normandale Community College",
+    "city": "Bloomington, MN",
+    "location": "9710 County Rd 17 Bloomington, Minnesota, Bloomington, MN",
+    "geoLocation": "44.828827,-93.3294897",
+    "degree": "CS degree",
+    "major": "Computer Science",
+    "dates": "2014-current",
+    "url": "http://www.normandale.edu"
+  },
+  {
+  "name": "University of Minnesota",
+  "city": "Minneapolis, MN",
+  "location": "University of Minnesota, Minneapolis, MN 55455",
+  "degree": "BA",
+  "major": "English",
+  "minor": "Japanese",
+  "dates": "2009-2011",
+  "url": "http://www.umn.edu"
+   },
+   {
+    "name": "Inver Hills Community College",
+    "city": "Inver Grove Heights, MN",
+    "location": "Inver Hills Community College, Inver Grove Heights, MN",
+    "degree": "AA",
+    "major": "Generals:MNtransfer curriculum",
+    "dates": "2006-2008",
+    "url": "https://inverhills.edu/"
+   }
+  ],
+  "onlineSchools": [
+  {
+    "title": "Intro to Computer Science",
+    "school": "Udacity",
+    "dates": "2014",
+    "url": "https://www.udacity.com/course/cs101",
+    "description": "Used python to learn how to build a search engine"
+  },
+  {
+    "title": "Javascript Syntax",
+    "school": "Udacity",
+    "dates": "2014",
+    "url": "https://www.udacity.com/course/ud804",
+    "description": ""
+  },
+  {
+    "title": "Intro to HTML and CSS",
+    "school": "Udacity",
+    "dates": "2014",
+    "url": "https://www.udacity.com/course/ud304"
+  }
+  ]
+};
+
+education.display = function() {
+  // schools
+  for (var school in education.schools) {
+    $("#education").append(HTMLschoolStart);
+    var formattedName =  HTMLschoolName.replace("%data%",education.schools[school].name);
+    formattedName = formattedName.replace("%url%",education.schools[school].url);
+    $(".education-entry:last").append(formattedName);
+    var formattedDegree =  HTMLschoolDegree.replace("%data%",education.schools[school].degree);
+    $(".education-entry:last").append(formattedDegree);
+    var formattedDates =  HTMLschoolDates.replace("%data%",education.schools[school].dates);
+    $(".education-entry:last").append(formattedDates);
+    var formattedLocation =  HTMLschoolLocation.replace("%data%",education.schools[school].city);
+    $(".education-entry:last").append(formattedLocation);
+    var formattedMajor =  HTMLschoolMajor.replace("%data%",education.schools[school].major);
+    // if the object doesn't have a minor then it will skip to major
+    if (education.schools[school].minor) {
+      var formattedMinor =  HTMLschoolMinor.replace("%data%",education.schools[school].minor);
+      var formattedMajorMinor = formattedMajor + formattedMinor;
+      $(".education-entry:last").append(formattedMajorMinor);
+    }
+    else {
+      $(".education-entry:last").append(formattedMajor);
+    }
+  };
+  // onlineSchools
+  $(".education-entry:last").append(HTMLonlineClasses);
+  for (var school in education.onlineSchools) {
+    var formattedTitle = HTMLonlineTitle.replace("%data%",education.onlineSchools[school].title);
+    $(".education-entry:last").append(formattedTitle);
+    var formattedSchool = HTMLonlineSchool.replace("%data%",education.onlineSchools[school].school);
+    $(".education-entry:last").append(formattedSchool);
+    var formattedDates = HTMLonlineDates.replace("%data%",education.onlineSchools[school].dates);
+    $(".education-entry:last").append(formattedDates);
+    var formattedDescription = HTMLonlineDescription.replace("%data%", education.onlineSchools[school].description);
+    $(".education-entry:last").append(formattedDescription);
+  }
+};
+
+education.display();
+
+function inName(name) {
+    name = name.split(" ")
+    console.log(name);
+    name[1] = name[1].toUpperCase(); 
+    name[0] = name[0][0].toUpperCase() + name[0].slice(1).toLowerCase();
+    return name[0] + " " + name[1];
+}
+
+
+$("#name").append(internationalizeButton);
+
+$(document).click(function(loc) {
+    var x = loc.pageX;
+    var y = loc.pageY;
+
+    logClicks(x,y);
+});
+
+$("#mapDiv").append(googleMap);
+
+//testing
+/*
+function initializePano() {
+  // var fenway = new google.maps.LatLng(42.345573, -71.098326);
+  var fenway = new google.maps.LatLng(address);
+
+  var panoramaOptions = {
+    position: fenway,
+    pov: {
+      heading: 34,
+      pitch: 10
+    }
+  };
+  var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
+  map.setStreetView(panorama);
+}
+*/
+//google.maps.event.addDomListener(window, 'load', initializePano); 
+
+//var map;
+//var berkeley = new google.maps.LatLng(44.828827,-93.3294897);
+/*
+var sv = new google.maps.StreetViewService();
+
+var panorama;
+
+function initializePano() {
+
+  panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'));
+
+  // Set up the map
+  /*
+  var mapOptions = {
+    center: berkeley,
+    zoom: 16,
+    streetViewControl: false
+  };
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  */
+
+  // getPanoramaByLocation will return the nearest pano when the
+  // given radius is 50 meters or less.
+  /*
+  google.maps.event.addListener(map, 'click', function(event) {
+      sv.getPanoramaByLocation(event.latLng, 50, processSVData);
+  });
+}
+
+function processSVData(data, status) {
+  if (status == google.maps.StreetViewStatus.OK) {
+    var marker = new google.maps.Marker({
+      position: data.location.latLng,
+      map: map,
+      title: data.location.description
+    });
+
+    panorama.setPano(data.location.pano);
+    panorama.setPov({
+      heading: 270,
+      pitch: 0
+    });
+    panorama.setVisible(true);
+
+    google.maps.event.addListener(marker, 'click', function() {
+
+      var markerPanoID = data.location.pano;
+      // Set the Pano to use the passed panoID
+      panorama.setPano(markerPanoID);
+      panorama.setPov({
+        heading: 270,
+        pitch: 0
+      });
+      panorama.setVisible(true);
+    });
+  } else {
+    alert('Street View data not found for this location.');
+  }
+}
+
+//google.maps.event.addDomListener(window, 'load', initializePano); */
+
+function svBackground(address) {
+  // streetviewUrl gets the url where the streetview is located
+  var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=640x400&location=' + address + '&pitch=-0.76' + '';
+  console.log(streetviewUrl);
+  var formattedImg = HTMLmapbgImg.replace('%data%', streetviewUrl); // replaces data
+  $('body').append(formattedImg); // appends it to body
+}
